24e30830340818c68519b97a3ae4d904676ef64a
Xaver Hellauer <xaver@hellauer.bayern>
2017-04-16 00:49:08 +0200

    v 0.1.0

--- create.js
+++ create.js
@@ -12,26 +12,32 @@ const themeName = 'Ysgrifennwr'
 const defaultsName = 'Ysgrifennwr Default Colors'
 const baseName = 'Ysgrifennwr Base'
 const blueLagoon = '#005577'
-const grey10 = '#1E1E1E'
+const floralWhite = '#F9F8F4'
 const deepCerise = '#E32791'
-const shamrock = '#30C798'
-const chenin = '#E3C472'
-const cornflowerBlue = '#6796E6'
-const plum = '#E59FDF'
-const riptide = '#81D8D0'
-const grey30 = '#515151'
-const grey90 = '#E5E6E6'
+const laPalma = '#488432'
+const goldenBrown = '#A25D0E'
+const ceruleanBlue = '#2C65B5'
+const violetBlue = '#B062A7'
+const lightSeaGreen = '#27BBBE'
+const grey70 = '#B8B8B8'
+const grey20 = '#424242'
 const alizarin = '#ED2939'
-const mediumAquamarine = '#6CD1B2'
-const sinbad = '#A2DCD7'
-const frenchLilac = '#E5B6E1'
-const doubleColonialWhite = '#E4CF98'
-const grey60 = '#969696'
-// const goldenBrown = '#AF5E00'
+const parsley = '#325D23'
+const atoll = '#1B8486'
+const rawUmber = '#71410A'
+const grey60 = '#999999'
 // const scarletGum = '#442059'
 // const deepSkyBlue = '#20BBFC'
 
 /* TODO:
+  // usually missing
+  {
+    "settings": {
+    "foreground": "#424242",
+    "background": "#f9f8f4"
+    }
+  },
+
     "editorInactiveSelection": "#3A3D41",
     "editorIndentGuides": "#404040",
     "editorSelectionHighlight": "#add6ff26"
@@ -39,7 +45,7 @@ const grey60 = '#969696'
     "statusBarBackground": blueLagoon,
     "statusBarDebuggingBackground": goldenBrown,
     "statusBarNoFolderBackground": scarletGum,
-    "statusBarForeground": grey90
+    "statusBarForeground": grey20
 */
 
 const options = {
@@ -48,8 +54,8 @@ const options = {
     'themes/*.json'
   ],
 
-  from: [/Dark\+ \(default dark\)/g, /Dark Default Colors/g, /Dark \(Visual Studio\)/g, /#c586c0/gi, /#569cd6/gi, /#4ec9b0/gi, /#ce9178/gi, /#608b4e/gi, /#d16969/gi, /#dcdcaa/gi, /#f44747/gi, /#000080/g, /#9cdcfe/gi, /#d4d4d4/gi, /#e1e1e1/gi, /#b5cea8/gi, /#646695/g, /#d7ba7d/gi, /#808080/g],
-  to: [themeName, defaultsName, baseName, deepCerise, cornflowerBlue, riptide, shamrock, grey30, mediumAquamarine, chenin, alizarin, blueLagoon, plum, grey90, grey10, sinbad, frenchLilac, doubleColonialWhite, grey60]
+  from: [/Light\+ \(default light\)/g, /Light Default Colors/g, /Light \(Visual Studio\)/g, /#af00db/gi, /#0000ff/gi, /#267f99/gi, /#a31515/gi, /#008000/g, /#811f3f/gi, /#795E26/gi, /#cd3131/gi, /#000080/g, /#001080/g, /#000000/g, /#ffffff/gi, /#09885a/gi, /#d7ba7d/gi, /#800000/g],
+  to: [themeName, defaultsName, baseName, deepCerise, ceruleanBlue, lightSeaGreen, laPalma, grey70, parsley, goldenBrown, alizarin, blueLagoon, violetBlue, grey20, floralWhite, atoll, rawUmber, grey60]
 }
 
 replace(options)
