7ee6c4d6f10d82fb7aefd5b827b207fff743ba4a
Xaver <software@hellauer.bayern>
2017-05-22 13:46:38 +0200

    fix css colors

--- create.js
+++ create.js
@@ -17,9 +17,11 @@ const grey70 = '#B8B8B8'
 const grey20 = '#424242'
 const alizarin = '#ED2939'
 const parsley = '#325D23'
+const eminence = '#7B4474'
 const atoll = '#1B8486'
 const rawUmber = '#71410A'
 const grey60 = '#999999'
+const bahamaBlue = '#1F477F'
 // const scarletGum = '#442059'
 // const deepSkyBlue = '#20BBFC'
 
@@ -29,8 +31,8 @@ const options = {
     'themes/*.json'
   ],
 
-  from: [/#af00db/gi, /#0000ff/gi, /#267f99/gi, /#a31515/gi, /#008000/g, /#811f3f/gi, /#795E26/gi, /#cd3131/gi, /#000080/g, /#001080/g, /#000000/g, /#ffffff/gi, /#09885a/gi, /#d7ba7d/gi, /#800000/g],
-  to: [deepCerise, ceruleanBlue, lightSeaGreen, laPalma, grey70, parsley, goldenBrown, alizarin, blueLagoon, violetBlue, grey20, floralWhite, atoll, rawUmber, grey60]
+  from: [/#af00db/gi, /#0000ff/gi, /#267f99/gi, /#a31515/gi, /#008000/g, /#811f3f/gi, /#795E26/gi, /#cd3131/gi, /#000080/g, /#001080/g, /#000000/g, /#ffffff/gi, /#09885a/gi, /#d7ba7d/gi, /#800000/g, /#ff0000/gi],
+  to: [deepCerise, ceruleanBlue, lightSeaGreen, laPalma, grey70, parsley, goldenBrown, alizarin, blueLagoon, violetBlue, grey20, floralWhite, atoll, rawUmber, bahamaBlue, eminence]
 }
 
 replace(options)
