8f33352522950619cb2597b94ea210a5e9e0c789
Xaver Hellauer <xaver.hellauer@gmail.com>
2020-07-16 18:38:48 +0200

    simplify prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,6 @@
 module.exports = {
-	tabWidth: 2,
-	semi: false,
-	singleQuote: true,
-	useTabs: true,
-	printWidth: 100,
-	overrides: [
-		{
-			files: ['*.md', '*.markdown'],
-			options: {
-				tabWidth: 2,
-				useTabs: false
-			}
-		}
-	]
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
 }
