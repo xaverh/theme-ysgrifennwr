d36d3f152543df1ed0acfeee85a54fbad6e6c85b
Xaver Hellauer <xaver.hellauer@gmail.com>
2020-07-16 18:35:45 +0200

    experimental changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
-	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-	env: {
-		es6: true,
-		node: true
-	},
-	rules: {
-		'prettier/prettier': 'off',
-		'no-unused-vars': 'warn'
-	}
+  extends: ['standard', 'prettier', 'prettier/standard', 'eslint:recommended'],
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
 }
+
+var a = Math.random() > 0.5 ? true : false
+if (a == f
--- normalize.js
+++ normalize.js
@@ -5,44 +5,52 @@ var theme = JSONC.parse(fs.readFileSync('./dump.json', 'utf8'))
 
 var byScope = {}
 theme.tokenColors.forEach(element => {
-	if (Array.isArray(element.scope)) {
-		element.scope.forEach(sc => {
-			if (byScope[sc] == null) byScope[sc] = {}
-			if (element.settings.foreground != null) byScope[sc].foreground = element.settings.foreground
-			if (element.settings.fontStyle != null) byScope[sc].fontStyle = element.settings.fontStyle
-		})
-	} else {
-		if (byScope[element.scope] == null) byScope[element.scope] = {}
-		if (element.settings.foreground != null)
-			byScope[element.scope].foreground = element.settings.foreground
-		if (element.settings.fontStyle != null)
-			byScope[element.scope].fontStyle = element.settings.fontStyle
-	}
+  if (Array.isArray(element.scope)) {
+    element.scope.forEach(sc => {
+      if (byScope[sc] == null) byScope[sc] = {}
+      if (element.settings.foreground != null)
+        byScope[sc].foreground = element.settings.foreground
+      if (element.settings.fontStyle != null)
+        byScope[sc].fontStyle = element.settings.fontStyle
+    })
+  } else {
+    if (byScope[element.scope] == null) byScope[element.scope] = {}
+    if (element.settings.foreground != null)
+      byScope[element.scope].foreground = element.settings.foreground
+    if (element.settings.fontStyle != null)
+      byScope[element.scope].fontStyle = element.settings.fontStyle
+  }
 })
 
 var bySettings = {}
 bySettings.foreground = {}
 bySettings.fontStyle = {}
 Object.keys(byScope).forEach(key => {
-	if (byScope[key].fontStyle != null) {
-		if (!Array.isArray(bySettings.fontStyle[byScope[key].fontStyle]))
-			bySettings.fontStyle[byScope[key].fontStyle] = []
-		bySettings.fontStyle[byScope[key].fontStyle].push(key)
-	}
-	if (byScope[key].foreground != null) {
-		if (!Array.isArray(bySettings.foreground[byScope[key].foreground]))
-			bySettings.foreground[byScope[key].foreground] = []
-		bySettings.foreground[byScope[key].foreground].push(key)
-	}
+  if (byScope[key].fontStyle != null) {
+    if (!Array.isArray(bySettings.fontStyle[byScope[key].fontStyle]))
+      bySettings.fontStyle[byScope[key].fontStyle] = []
+    bySettings.fontStyle[byScope[key].fontStyle].push(key)
+  }
+  if (byScope[key].foreground != null) {
+    if (!Array.isArray(bySettings.foreground[byScope[key].foreground]))
+      bySettings.foreground[byScope[key].foreground] = []
+    bySettings.foreground[byScope[key].foreground].push(key)
+  }
 })
 
 var normalized = []
 Object.keys(bySettings.fontStyle).forEach(key => {
-	normalized.push({ scope: bySettings.fontStyle[key], settings: { fontStyle: key } })
+  normalized.push({
+    scope: bySettings.fontStyle[key],
+    settings: { fontStyle: key },
+  })
 })
 
 Object.keys(bySettings.foreground).forEach(key => {
-	normalized.push({ scope: bySettings.foreground[key], settings: { foreground: key } })
+  normalized.push({
+    scope: bySettings.foreground[key],
+    settings: { foreground: key },
+  })
 })
 
 theme.tokenColors = normalized
